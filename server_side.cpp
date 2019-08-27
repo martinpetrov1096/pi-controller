1d3b44a69afedda1f6172b2a6823f2f4dc5125d2
Martin Petrov <mlpetrov@ucdavis.edu>
2019-08-28 00:13:19 +0300

    debug

--- server_side.cpp
+++ server_side.cpp
@@ -45,7 +45,7 @@ int main() {
                 //cout << "Fork worked\n child_pid: " << currProc.pid << endl;
                 write(0, "Fork Worked\n", 15);
                 chdir("/home/pi/Documents/pi-controller/");
-                execl("/home/pi/Documents/pi-controller/tower_fan", "./tower_fan" , (char*) NULL);
+                execl( ("/home/pi/Documents/pi-controller/" + currProc.name).c_str, "./tower_fan" , (char*) NULL);
 
             }
     }
