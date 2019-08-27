2517fa849001009c521a3c909142165272c99624
Martin Petrov <mlpetrov@ucdavis.edu>
2019-08-27 23:21:55 +0300

    added makefile

---
+++ Makefile
@@ -0,0 +1,7 @@
+all: tower_main tower_fan
+
+tower_main:
+	g++ -std=c++11 -o tower_main server_side.cpp
+
+tower_fan:
+	g++ -std=c++11 -o tower_fan fan_client_side.cpp
--- server_side.cpp
+++ server_side.cpp
@@ -45,7 +45,7 @@ int main() {
                 //cout << "Fork worked\n child_pid: " << currProc.pid << endl;
                 write(0, "Fork Worked\n", 15);
                 chdir("/home/pi/Documents/pi-controller/");
-                execl(currProc.name.c_str, ("./" + currProc.name).c_str, (char*) NULL);
+                execl(currProc.name.c_str(), ("./" + currProc.name).c_str(), (char*) NULL);
 
             }
     }
