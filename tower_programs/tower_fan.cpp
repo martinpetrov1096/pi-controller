728afc66abaea443b5f7a88615f22ebfb1165e11
Martin Petrov <mlpetrov@ucdavis.edu>
2019-09-04 14:52:27 +0100

    Progress on tower-side

--- tower_programs/tower_fan.cpp
+++ tower_programs/tower_fan.cpp
@@ -7,38 +7,49 @@ cmd args:
 
  -a : Auto (Default) - Uses fan speed from tower_sys_info to determine ap
  -m : Manual, followed by an int specifying the percentage of the fan speed you want
+
+ 
  -s : return current fan speed
 */
 #include <iostream>
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include<termios.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <stdlib.h>
+
+
+
+#include <fcntl.h>  //file open types
 #include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+
 
 using namespace std;
 
 int main (int argc, char *argv[]) {
 
-    int fd;   
-    const char temp[] = "50";
-     
+    int fd; //file descriptor to be used for serial port
+    //Open the serial port 
     if ((fd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK)) < 0) {
         printf("Open failed with exit code: %d\n", fd);
         return -1;
     }
 
-    if(write (fd, &temp, strlen(temp)) < 0) {
-            cout << "fail" << endl;
-            return -1;
+    //If 1st arg is a number, write the value, else, fan will be in auto
+    if (strtol(argv[1], NULL, 10) != 0) {
+
+        if(write (fd, argv[1], strlen(argv[1])) < 0) {
+                return -1;
+        }
+
+    } else {
+
+        if(write (fd, "auto", 4) < 0) {
+                return -1;
+        }
+
     }
-    
+        
     close(fd);
-    
+
 }
--- tower_programs/tower_main.cpp
+++ tower_programs/tower_main.cpp
@@ -9,11 +9,13 @@ Usage:
     tower_main -l blue - Sets the LED's to blue
     tower_main -l white
 
+    tower_main -f auto -l blue - sets fan to auto and led's to blue
+
 */
 
 #include <stdio.h>
 #include <unistd.h>
-
+#include <sys/wait.h>
 #include <string>
 
 using namespace std;
@@ -26,12 +28,13 @@ int main(int argc, char *argv[]) {
         switch(opt)  
         {  
             case 'f': {
-                printf("Fan Mode: %c\n", opt); 
-                printf("Fan Speed: %s\n", optarg);  
-
+                printf("Calling fan with options: %s\n", optarg);  
                 int childPid = fork();
                 if (childPid == 0) {
                     execl("/home/pi/Documents/pi-controller/tower_programs/tower_fan", "./tower_fan", optarg, (char*) 0);
+                } else {
+                    wait(&childPid);
+                    return 0;
                 }
             }
                 break;  
@@ -56,18 +59,4 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-
-
-
-/*
-New idea: looks exactly like the standard bash program in the book
-
-3 children:
-FAN - Send percentage of fan, exit when done
-SYSINFO - Runs in the background, might not even be needed in this program, start when tower_main starts,
-close when tower_main is closed. Send CPU Temp
-LED - Send led command, exit when done
-
-
-*/
 
