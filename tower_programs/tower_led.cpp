f967aa4f0e6cd1a24e8266f44c62ecca612a06f2
Martin Petrov <mlpetrov@ucdavis.edu>
2019-09-24 03:32:32 +0100

    changed input style of tower_led

--- tower_programs/tower_led.cpp
+++ tower_programs/tower_led.cpp
@@ -45,6 +45,28 @@ int main (int argc, char *argv[]) {
 
     //If 2nd arg is a number, write the value, else, fan write -1 to let the fan know to be in auto
     //Add "\n" to be able to parse on the pi's side
+    if (arg == "blue") {
+        if(write (fd, "0 0 100 100\n", 12) < 0) {
+                return -1;
+        }
+    } else if (arg == "red") {
+        if(write (fd, "100 0 0 100\n", 12) < 0) {
+                return -1;
+        }
+    } else if (arg == "green") {
+        if(write (fd, "0 100 0 100\n", 12) < 0) {
+                return -1;
+        }
+    } else if (arg == "white") {
+        if(write (fd, "100 100 100 100\n", 12) < 0) {
+                return -1;
+        }
+    }
+    
+    
+    
+    
+    
     if (strtol(arg, NULL, 10) != 0) {
 
         strcat(arg, "\n");
